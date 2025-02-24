d0a596a1050f33d55ec8130754d3cd73b402a2f9
Dtsnko <right0346@gmail.com>
2025-02-24 16:51:05 +0200

    [WTEL-4909] changes: - bugfix now needed read permission to push comments to the case

--- internal/app/case_comment.go
+++ internal/app/case_comment.go
@@ -210,13 +210,13 @@ func (c *CaseCommentService) PublishComment(
 	createOpts.ParentID = tag.GetOid()
 	logAttributes := slog.Group("context", slog.Int64("user_id", createOpts.GetAuthOpts().GetUserId()), slog.Int64("domain_id", createOpts.GetAuthOpts().GetDomainId()), slog.Int64("case_id", tag.GetOid()))
 
-	accessMode := auth.Edit
+	accessMode := auth.Read
 	if !createOpts.GetAuthOpts().CheckObacAccess(CaseCommentMetadata.GetParentScopeName(), accessMode) {
 		slog.ErrorContext(ctx, "user doesn't have required (EDIT) access to the case", logAttributes)
 		return nil, AppForbiddenError
 	}
 	if createOpts.GetAuthOpts().IsRbacCheckRequired(CaseCommentMetadata.GetParentScopeName(), accessMode) {
-		access, err := c.app.Store.Case().CheckRbacAccess(createOpts, createOpts.GetAuthOpts(), auth.Edit, createOpts.ParentID)
+		access, err := c.app.Store.Case().CheckRbacAccess(createOpts, createOpts.GetAuthOpts(), accessMode, createOpts.ParentID)
 		if err != nil {
 			slog.ErrorContext(ctx, err.Error(), logAttributes)
 			return nil, AppForbiddenError
